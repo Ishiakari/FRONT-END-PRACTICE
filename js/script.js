896056c13f99463ddf6e77f46cacb800b72a20f7
Zach Alfred <arevalo.zachalfred123@gmail.com>
2026-03-01 11:27:52 +0800

    with clear button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 let countEl = document.getElementById("count-el")
 let count = 0
-
-
+let saveEl = document.getElementById("save-el")
 
 function increment() {
-    count = count + 1 
+    count += 1
     countEl.innerText = count
     
 }
 
 function save(){
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = count
 }
-
-save()
+function clearAll(){
+    count = 0
+    countEl.textContent = count
+    saveEl.textContent = "Previous Entries: "
+}
